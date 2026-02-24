7aa9cdb81e878d5fe2cc2f8ecbcfe7220bdd12b8
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-02-24 08:32:27 -0500

    Update setup-windows.bat

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -16,5 +16,6 @@ haxelib install hxvlc 1.5.5
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib install hxvlc 1.5.5
 echo Finished!
 pause
